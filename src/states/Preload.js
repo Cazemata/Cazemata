276dd5b4f630a67a1ca1f946cf9a4b1ac849c290
Razvan Sfirlogea <razvan.sfirlogea@gmail.com>
2024-03-30 06:58:55 +0200

    .

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -5,13 +5,13 @@ class Preload extends Phaser.State {
         //this.game.load.image('myImage', 'assets/my-image.png');
         //this.game.load.audio('myAudio', 'assets/my-audio.wav');
         //this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
-        this.game.load.image('back', '../static/assets/ilu_bg.png');
-        this.game.load.image('mid3', '../static/assets/ilu_03.png');
-        this.game.load.image('mid2', '../static/assets/ilu_02.png');
-        this.game.load.image('mid1', '../static/assets/ilu_01.png');
-        this.game.load.image('man', '../static/assets/ilu_man.png');
-        this.game.load.image('sun', '../static/assets/sun.png');
-        this.game.load.image('moon', '../static/assets/moon.png');
+        this.game.load.image('back', './static/assets/ilu_bg.png');
+        this.game.load.image('mid3', './static/assets/ilu_03.png');
+        this.game.load.image('mid2', './static/assets/ilu_02.png');
+        this.game.load.image('mid1', './static/assets/ilu_01.png');
+        this.game.load.image('man', './static/assets/ilu_man.png');
+        this.game.load.image('sun', './static/assets/sun.png');
+        this.game.load.image('moon', './static/assets/moon.png');
     }
 
     create() {
